e54d9f344c9cc524d3b07978f6f4518ad0f8f564
wfairmanOlin <wfairman@olin.edu>
2025-08-14 14:16:55 -0400

    initial commit

---
+++ winch_companion/main.py
@@ -0,0 +1,52 @@
+'''
+set stream rate on an APM
+'''
+from builtins import range
+import sys
+from pymavlink import mavutil
+import time
+
+def wait_heartbeat(m):
+    '''wait for a heartbeat so we know the target system IDs'''
+    print("Waiting for APM heartbeat")
+    m.wait_heartbeat()
+    print("Heartbeat from APM (system %u component %u)" % (m.target_system, m.target_system))
+
+def show_messages(m):
+    '''show incoming mavlink messages'''
+    while True:
+        msg = m.recv_match(blocking=True)
+        if not msg:
+            return
+        elif msg.get_type() == "BAD_DATA":
+            pass
+        elif msg.get_Type() == "":
+            pass
+        else:
+            print(msg)
+
+# create a mavlink serial instance
+master = mavutil.mavlink_connection('/dev/serial0', baud=115200)
+
+# wait for the heartbeat msg to find the system ID
+wait_heartbeat(master)
+
+# stream_rate = 1
+# print("Sending all stream request for rate %u" % stream_rate)
+# for i in range(0, 3):
+#     master.mav.request_data_stream_send(master.target_system, master.target_component,
+#                                         mavutil.mavlink.MAV_DATA_STREAM_ALL, stream_rate, 1)
+
+# show_messages(master)
+
+print("starting")
+last_time = time.time()
+
+test_data = [i for i in range(96)]
+while True:
+    if (time.time() - last_time) > 1:
+        last_time = time.time()
+        master.mav.heartbeat_send(mavutil.mavlink.MAV_TYPE_WINCH, mavutil.mavlink.MAV_AUTOPILOT_INVALID, mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED, 0, mavutil.mavlink.MAV_STATE_ACTIVE)
+        master.mav.winch_status_send(0,1,2,3,4,5,6,7)
+        master.mav.data96_send(mavutil.mavlink.MAV_TYPE_WINCH, 3, test_data)
+
---
+++ winch_companion/winch_benchtest.py
@@ -0,0 +1,158 @@
+import threading
+import ADS1x15
+from gpiozero.pins.pigpio import PiGPIOFactory
+from gpiozero import Servo
+import time
+import logging
+import math
+
+#Initialize Servo
+servo = Servo(18, pin_factory=PiGPIOFactory())
+servo.value = 0
+#Initialize ADC
+adc = ADS1x15.ADS1115(1)
+adc.setGain(1)
+time.sleep(0.05)
+#hall effect settings (calibrate for every system)
+HALL_MIN = 1035
+HALL_MAX = 12285
+
+#global variables
+CYCLE_COUNT = 0
+CYCLE_LIMIT = 0
+RETRACTED = 1
+AUTO_STATE = "idle"
+AUTO_DROP = 15 #drop time for auto cycling
+AUTO_PWR = 1.0 #retrieve power
+ROTATION = -1 #CW, -1 CCW
+
+##### LOGGING #####
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s', filename='winchlog.log', encoding='utf-8',
+                    level=logging.INFO)
+logger = logging.getLogger(__name__)
+logger.info('Starting')
+
+def release():
+    servo.value = -ROTATION * 0.4
+    safety_timer = time.time()
+    #TODO: Check BLE connection to determine location of probe
+    while (RETRACTED == 1):
+        if (time.time() - safety_timer) > 0.7:
+            servo.value = 0
+            print("safety timer triggered")
+            logger.warning("safety timer on release")
+            break
+        pass
+    servo.value = 0
+
+def winch_control():
+    global RETRACTED
+    dist = adc.readADC(0)
+    target_dist = HALL_MIN
+    pwr_limit = 0.3
+    while True:
+        try:
+            dist = adc.readADC(0) - target_dist
+        except:
+            servo.value = 0
+            logger.warning('failed to read hall effect')
+            time.sleep(5)
+        
+        pwr = dist / (HALL_MAX - HALL_MIN)
+        pwr = pwr * pwr_limit
+        if pwr > 0:
+            pwr = math.pow(pwr,1/3)
+        
+
+        #limit min/max power
+        if pwr > pwr_limit:
+            pwr = pwr_limit
+        elif pwr < 0:
+            pwr = 0
+                
+        if (dist < (target_dist + 50)):
+            RETRACTED = 1
+            if (ROTATION * servo.value) > 0.0:
+                servo.value = 0
+        elif (dist < 10_000) and ((ROTATION * servo.value) > 0.0):
+            servo.value = ROTATION * pwr
+        elif (dist > 8_000):
+            if RETRACTED != 0:
+                RETRACTED = 0
+
+def state_machine():
+    global RETRACTED, CYCLE_COUNT, CYCLE_LIMIT
+    global AUTO_STATE, AUTO_PWR
+    drop_timer = time.time()
+    retrieve_timer = time.time()
+    while True:
+        if AUTO_STATE == "idle":
+            pass
+        elif AUTO_STATE == "released":
+            if AUTO_DROP < (time.time() - drop_timer):
+                if RETRACTED == 0:
+                    AUTO_STATE = "retrieving"
+                    print("retrieve started")
+                    logger.info("retrieve started")
+                    servo.value = ROTATION * AUTO_PWR
+                    retrieve_timer = time.time()
+                else :
+                    print("release failed")
+                    logger.warning("release failed")
+                    AUTO_STATE = "idle"
+
+        elif AUTO_STATE == "retrieving":
+            if RETRACTED:
+                AUTO_STATE = "retracted"
+                CYCLE_COUNT += 1
+                print(f"finished cycle {CYCLE_COUNT} of {CYCLE_LIMIT}")
+                logger.info("cylce finished: %s", CYCLE_COUNT)
+                if CYCLE_COUNT >= CYCLE_LIMIT:
+                    AUTO_STATE = "idle"
+                    print(f"program ended at {CYCLE_COUNT} cycles")
+                    logger.info("program ended at %s cycles", CYCLE_COUNT)
+                time.sleep(10)
+            elif 35 < (time.time() - retrieve_timer):
+                servo.value = 0
+                AUTO_STATE = "idle"
+                logger.warning("ran out of time to retrieve")
+                print("ran out of time to retrieve")
+
+        elif AUTO_STATE == "retracted":
+            release()
+            drop_timer = time.time()
+            AUTO_STATE = "released"
+
+wcontrol = threading.Thread(target=winch_control)
+smachine = threading.Thread(target=state_machine)
+wcontrol.start()
+smachine.start()
+while True:
+   
+   #handle user inputs
+    cmd = input()
+    cmd = cmd.split(',')
+    if len(cmd) < 1:
+        continue
+    elif (len(cmd) == 2) and (cmd[0] == 's'):
+        print("setting servo to: ", float(cmd[1]))
+        servo.value = float(cmd[1])
+    elif cmd[0] == 'q':
+        print("stopping")
+        AUTO_STATE = "idle"
+        servo.value = 0
+    elif cmd[0] == 'r':
+        print("releasing")
+        release()
+    elif cmd[0] == 'p':
+        print(f"servo pwr: {servo.value}")
+        print(f"hall efct: {adc.readADC(0)}")
+        print(f"    state: {AUTO_STATE}")
+        print(f"retracted: {'yes' if RETRACTED == 1 else 'no'}")
+    elif cmd[0] == 'start':
+        CYCLE_COUNT = 0
+        CYCLE_LIMIT = 1
+        if len(cmd) == 2:
+            CYCLE_LIMIT = int(cmd[1])
+        print(f"running test for {CYCLE_LIMIT} cycles")
+        AUTO_STATE = "retracted"
